f0f158d484e1b4f2df0e56b26f75c78cafe04287
dPerez015 <davidperezgallego@enti.cat>
2018-12-03 23:58:52 +0100

    mas eventsystem

--- LudumDare2018/Assets/Scripts/HUDTextController.cs
+++ LudumDare2018/Assets/Scripts/HUDTextController.cs
@@ -10,16 +10,18 @@ public class HUDTextController : MonoBehaviour {
 
     GameObject levelManager;
     PlayerManager player;
+    int initialAlum;
 	// Use this for initialization
 	void Start () {
         levelManager = GameObject.Find("Level Manager");
         int currentLevel=levelManager.GetComponent<LevelManager>().getLevelNumber();
         level.text = (currentLevel+1).ToString() + "/15";
         player = levelManager.GetComponent<LevelManager>().getCharacter();
+        initialAlum = Timemanager.Instance.totalNumberOfAlum;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        alumnos.text = player.numOfAlumn.ToString();
+        alumnos.text = (player.numOfAlumn + initialAlum).ToString();
 	}
 }
