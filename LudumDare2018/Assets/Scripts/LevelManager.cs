1ee5a69dbee9cc62c164c9b58f41e4d1f59579ef
dPerez015 <davidperezgallego@enti.cat>
2018-12-03 05:35:37 +0100

    -Props - Level management - Menus

---
+++ LudumDare2018/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public enum State {Intro,Animation, Playing, End}
+    State state;
+    [Header("Level Number")]
+    public int currentLevel;
+    public bool isLastLevel;
+
+    [Header("Player")]
+    public GameObject Character;
+    public GameObject CharacterStartPosition;
+
+    [Header("Canvas")]
+    public Image fadeImg;
+
+    float startTime;
+
+    [Header("StartAnimation")]
+    public float IntroDuration=4;
+    public float fadeDuration=0.25f;
+    Vector3 initialPlayerPos;
+
+
+    [Header("EndAnimation")]
+    public ParticleSystem particles;
+    public GameObject endOfLevelGoal;
+    public float endAnimTime;
+
+    [Header("Menu")]
+    public MenuManager menu;
+    public GameObject PauseCanvas;
+
+    [Header("AlumnPrefab")]
+    public GameObject alumno;
+
+    private void Reset()
+    {
+        gameObject.name = "Level Manager";
+    }
+
+    public void spawnAlumno(Vector3 position, Quaternion rotation)
+    {
+        Instantiate(alumno, position, rotation);
+    }
+
+    void Start()
+    {
+        state = State.Intro;
+        Character.GetComponent<CircleCollider2D>().enabled = false;
+        initialPlayerPos = Character.transform.position = transform.position;
+        fadeImg.gameObject.SetActive(true);
+        menu.currentLevel = currentLevel;
+        PauseCanvas.SetActive(false);
+        startTime = 0;
+    }
+
+    void startGameplay()
+    {
+        state = State.Playing;
+        Character.GetComponent<CircleCollider2D>().enabled = true;
+        Character.GetComponent<PlayerManager>().started = true;
+        Debug.Log("start");
+        
+    }
+
+    public void endLevel()
+    {
+        Character.GetComponent<PlayerManager>().started = false;
+        Character.GetComponent<PlayerManager>().setVelocity(new Vector2(0, 0));
+        particles.Play();
+        startTime = 0;
+        initialPlayerPos = Character.transform.position;
+        fadeImg.color = new Color(0, 0, 0, 0);
+        state = State.End;
+    }
+
+
+    public void Update()
+    {
+        switch (state)
+        {
+            case State.Intro:
+                //activar la animacion del player
+                startTime += Time.deltaTime;
+                float timeProp = startTime / IntroDuration;
+                Character.transform.position = Vector3.Lerp(initialPlayerPos,CharacterStartPosition.transform.position,timeProp);
+                if (fadeImg.gameObject.activeInHierarchy) {
+                    fadeImg.color = new Color(0, 0, 0, Mathf.Clamp(1 - (startTime / fadeDuration),0,1));
+                    if (startTime >= fadeDuration)
+                        fadeImg.gameObject.SetActive(false);
+                }
+                if (startTime > IntroDuration)
+                {
+                    startGameplay();
+                }
+
+                break;
+            case State.Playing:
+                if (Input.GetKeyDown("escape"))
+                {
+                    PauseCanvas.SetActive(!PauseCanvas.activeInHierarchy);
+                }
+
+                break;
+            case State.End:
+                startTime += Time.deltaTime;
+                Character.transform.position = Vector3.Lerp(initialPlayerPos, endOfLevelGoal.transform.position, startTime / endAnimTime);
+                if (!fadeImg.gameObject.activeInHierarchy)
+                {
+                    if (startTime > endAnimTime - fadeDuration*2)
+                    {
+                        fadeImg.gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+
+                    fadeImg.color = new Color(0, 0, 0, Mathf.Clamp(1-(endAnimTime - startTime) / fadeDuration*2 , 0, 1));
+                }
+                if (startTime > endAnimTime)
+                {
+                    if (!isLastLevel)
+                        SceneManager.LoadScene("Level" + (currentLevel + 1).ToString());
+                    else
+                        SceneManager.LoadScene("MainMenu");
+                }
+                break;
+            
+        }
+        
+
+    }
+
+    
+
+}
--- LudumDare2018/Assets/Scripts/PlayerManager.cs
+++ LudumDare2018/Assets/Scripts/PlayerManager.cs
@@ -5,17 +5,19 @@ public class PlayerManager : MonoBehaviour {
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb2D;
     Camera cam;
+    LevelManager manager;
 
     //states
     bool clicked;
     bool moving;
+    public bool started;
 
-    //timebar
+    [Header("Timebar")]
     public Timebar timeBar;
     float timeLeft;
     public float maxTime;
 
-    //movement parameters
+    [Header("Movement parameters")]
     public float MaxDistance;
     float dragAcceleration;
     public float velocity;
@@ -29,14 +31,17 @@ public class PlayerManager : MonoBehaviour {
     Vector2 actualVelocity;
     Vector2 initialVelocity;
 
-    //mouse controls
-    Vector3 mouseInitialPos;
+    [Header("Controls")]
+    [Tooltip("Distancia maxima en pixeles que puedes mover el raton para ganar potencia de tiro, apartir de aqui ya no aumentara mas")]
     public float mouseMaxDrag;
+    Vector3 mouseInitialPos;
+    
     float mouseDragAmount;
 
-    //alumnos
+    [Header("Alumnos")]
+    [Tooltip("Porcentage de la distancia maxima que pierde por alumno recogido")]
+    public float dragAmount = 0.05f;
     int numOfAlumn;
-    float dragAmount = 0.05f;
 
     //arrow 
     DirectionArrow arrow;
@@ -48,14 +53,17 @@ public class PlayerManager : MonoBehaviour {
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb2D = GetComponent<Rigidbody2D>();
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();
-        
+        manager = GameObject.Find("Level Manager").GetComponent<LevelManager>();
+
         //set states to false
         clicked = false;
         moving = false;
+        started = false;
 
         //we get the times 
-        //TODO(should be changed to getting them from singleton)
-        maxTime = timeLeft = 100;
+        maxTime =  Timemanager.Instance.getMaxTime();
+        timeLeft = Timemanager.Instance.getCurrentTime();
+        timeBar.setHealthPercent(timeLeft / maxTime);
 
         arrow = transform.GetChild(0).gameObject.GetComponent<DirectionArrow>();
         arrow.SetActive(false);
@@ -66,68 +74,71 @@ public class PlayerManager : MonoBehaviour {
         dragAcceleration = -(vel * vel) / (2 * mDist);
     }
 
-    void setVelocity(Vector2 v){
+    public void setVelocity(Vector2 v){
         rb2D.velocity = v;
         actualVelocity = v;
     }
 
     private void Update()
     {
-        //clicked but not released
-        if (clicked){
-            //we check the direction and the percentage of the max amount a player
-            Vector3 direction = mouseInitialPos - Input.mousePosition;
-            mouseDragAmount = Mathf.Clamp(direction.magnitude/mouseMaxDrag,0,1);
-
-            //rotate the gameobject
-            transform.rotation = Quaternion.Euler(0,0,Mathf.Rad2Deg*Mathf.Atan2(direction.y, direction.x));
-
-            //scale the arrow
-            arrow.setScale(mouseDragAmount);
-
-            //we show the player how much will that movement cost
-            timeBar.showHealthLossPercent((timeLeft - (1*mouseDragAmount))/maxTime);
-
-            //cuando deja ir el click
-            if (Input.GetMouseButtonUp(0))
-            {
-                position = transform.position;
-                //cambiamos el color 
-                //TODO activar animación
-                spriteRenderer.color = new Color(1, 1, 1);
-                //seteamos la velocidad
-                setVelocity(direction.normalized * velocity);
-                initialVelocity=actualVelocity;
-
-                //calculamos el drag necesario para que se mueva solo esa distancia
-                calculateDragAccel(velocity, MaxDistance * mouseDragAmount*(1-(numOfAlumn*dragAmount)));
-
-                //cambiamos de estado
-                clicked = false;
-                moving = true;
-
-                //seteamos el tiempo inicial del moviemiento, necesario para los calculos del mrua
-                startMovementTime = Time.time;
-                
-                //we set the new time to the timebar
-                //TODO get this value from singleton
-                timeLeft-=1*mouseDragAmount;
-                timeBar.setHealthPercent(timeLeft/maxTime);
-
-                //desactivamos la flecha
-                arrow.SetActive(false);
-            }
-        }
-        //cuando se esta moviendo 
-        else if (moving)
+        if (started)
         {
-            //seteamos la velocidad según el tiempo que ha transcurrido desde el inicio
-            setVelocity(initialVelocity.normalized * (velocity + (dragAcceleration * (Time.time - startMovementTime))));
-            if (checkMovementStop())
+            //clicked but not released
+            if (clicked)
             {
-                Debug.Log((transform.position - position).magnitude);
-                moving = false;
-                setVelocity(new Vector2(0, 0));
+                //we check the direction and the percentage of the max amount a player
+                Vector3 direction = mouseInitialPos - Input.mousePosition;
+                mouseDragAmount = Mathf.Clamp(direction.magnitude / mouseMaxDrag, 0, 1);
+
+                //rotate the gameobject
+                transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(direction.y, direction.x));
+
+                //scale the arrow
+                arrow.setScale(mouseDragAmount);
+
+                //we show the player how much will that movement cost
+                timeBar.showHealthLossPercent((timeLeft - (1 * mouseDragAmount)) / maxTime);
+
+                //cuando deja ir el click
+                if (Input.GetMouseButtonUp(0))
+                {
+                    position = transform.position;
+                    //cambiamos el color 
+                    //TODO activar animación
+                    spriteRenderer.color = new Color(1, 1, 1);
+                    //seteamos la velocidad
+                    setVelocity(direction.normalized * velocity);
+                    initialVelocity = actualVelocity;
+
+                    //calculamos el drag necesario para que se mueva solo esa distancia
+                    calculateDragAccel(velocity, MaxDistance * mouseDragAmount * (1 - (numOfAlumn * dragAmount)));
+
+                    //cambiamos de estado
+                    clicked = false;
+                    moving = true;
+
+                    //seteamos el tiempo inicial del moviemiento, necesario para los calculos del mrua
+                    startMovementTime = Time.time;
+
+                    //we set the new time to the timebar
+                    timeLeft -=1 + MaxDistance * mouseDragAmount * (1-numOfAlumn *dragAmount);
+                    Timemanager.Instance.setCurrentTime(timeLeft);
+                    timeBar.setHealthPercent(timeLeft / maxTime);
+
+                    //desactivamos la flecha
+                    arrow.SetActive(false);
+                }
+            }
+            //cuando se esta moviendo 
+            else if (moving)
+            {
+                //seteamos la velocidad según el tiempo que ha transcurrido desde el inicio
+                setVelocity(initialVelocity.normalized * (velocity + (dragAcceleration * (Time.time - startMovementTime))));
+                if (checkMovementStop())
+                {
+                    moving = false;
+                    setVelocity(new Vector2(0, 0));
+                }
             }
         }
     }
@@ -144,7 +155,7 @@ public class PlayerManager : MonoBehaviour {
 
     private void OnMouseDown()
     {
-        if (!moving)
+        if (!moving && started)
         {
             spriteRenderer.color = new Color(0.5f, 0.5f, 0.5f);
             mouseInitialPos = cam.WorldToScreenPoint(transform.position);
@@ -163,19 +174,43 @@ public class PlayerManager : MonoBehaviour {
             //rotate the gameobject
             transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(actualVelocity.y, actualVelocity.x));
         }
-        
+        else if(collision.gameObject.tag == "Pinchos")
+        {
+            setVelocity(new Vector2(0, 0));
+            moving = false;
+        }
      }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Alumno")
         {
-
-
             float movementLeft = mouseDragAmount * MaxDistance * (1 - (dragAmount * numOfAlumn)) - (initialVelocity.magnitude * (Time.time - startMovementTime) + dragAcceleration *Mathf.Pow(Time.time-startMovementTime,2)/ 2);
             numOfAlumn++;
 
             calculateDragAccel(actualVelocity.magnitude, movementLeft);
-            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "EndOfLevel")
+        {
+            manager.endLevel();
+        }
+        
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "LoseAlum")
+        {
+            if (numOfAlumn > 0)
+            {
+                manager.spawnAlumno(collision.transform.position, transform.rotation);
+                float movementLeft = mouseDragAmount * MaxDistance * (1 - (dragAmount * numOfAlumn)) - (initialVelocity.magnitude * (Time.time - startMovementTime) + dragAcceleration * Mathf.Pow(Time.time - startMovementTime, 2) / 2);
+                numOfAlumn--;
+                calculateDragAccel(actualVelocity.magnitude, movementLeft);
+
+            }
         }
     }
 }
