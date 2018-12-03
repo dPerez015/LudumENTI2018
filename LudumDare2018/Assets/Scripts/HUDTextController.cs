7bbf9779751c867337ebece79da857d4f18b7354
David <davidperezgallego@enti.cat>
2018-12-03 21:36:08 +0100

    Working on punctuation and hud

---
+++ LudumDare2018/Assets/Scripts/HUDTextController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDTextController : MonoBehaviour {
+
+    Text alumnos;
+    Text level;
+
+    GameObject levelManager;
+	// Use this for initialization
+	void Start () {
+        levelManager = GameObject.Find("Level Manager");
+        levelManager.GetComponent<LevelManager>().getLevelNumber();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
